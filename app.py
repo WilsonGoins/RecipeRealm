f37a35102e26c3fcb746904a03777500d9529811
WilsonGoins <wilson.goins@ufl.edu>
2024-02-26 15:35:14 -0500

    Added functionality to app.py to return an HTML file when visiting a certain url

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, StreamingResponse
+
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root():
-    return {"message": "Hello World"}
+    htmlCode = ''
+    with open('templates/home.html') as file:
+        htmlCode = file.read()
+
+    return HTMLResponse(content=htmlCode, status_code=200)
