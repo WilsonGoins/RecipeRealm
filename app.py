01f3e6b3c5d3464ce169db8e6e0d7359f33613d1
WilsonGoins <wilson.goins@ufl.edu>
2024-02-27 16:46:43 -0500

    messing around with React. we may be cooked

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-import uvicorn
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, StreamingResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, Response
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/", response_class=HTMLResponse)
-async def root():
-    htmlCode = ''
-    with open('templates/home.html') as file:
-        htmlCode = file.read()
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
-    return HTMLResponse(content=htmlCode, status_code=200)
+
+# @app.get("/", response_class=HTMLResponse)
+# async def root():
+#     htmlCode = ''
+#     with open('templates/home.html') as file:
+#         htmlCode = file.read()
+#     return HTMLResponse(content=htmlCode, status_code=200)
+#
+#     # return templates.TemplateResponse("home.html")
+
+
+# @app.post("/login", response_class=HTMLResponse)
+# async def login():
+#     htmlCode = ''
+#     with open('templates/home.html') as file:
+#         htmlCode = file.read()
+#
+#     return HTMLResponse(content=htmlCode, status_code=200)
