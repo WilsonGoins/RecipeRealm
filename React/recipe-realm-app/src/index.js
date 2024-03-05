0bf0277848d5160a6128eb0ed932c28e21ef9ec8
WilsonGoins <wilson.goins@ufl.edu>
2024-03-05 02:20:55 -0500

    making progress on background image. back end isn't working???

--- React/recipe-realm-app/src/index.js
+++ React/recipe-realm-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Login from './Login'
+import Login from './Login/Login'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
