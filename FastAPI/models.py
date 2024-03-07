749268dd0ca0f6f92b39c47c44be6c14b1df7071
ajaypatel12345 <ajaypatel@ufl.edu>
2024-03-07 11:11:52 -0500

    test

---
+++ FastAPI/models.py
@@ -0,0 +1,13 @@
+from database import Base
+from sqlalchemy import Column, Integer, Boolean, String
+
+
+class Transaction(Base):
+    __tablename__ = "recipes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    calories = Column(Integer)
+    description = Column(String)
+    is_healthy = Column(Boolean)
+    date = Column(String)
