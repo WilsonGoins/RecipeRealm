6424d7170e635506843511c02f228a7e5ea86c37
WilsonGoins <wilson.goins@ufl.edu>
2024-03-07 14:39:05 -0500

    Huge progress. next to do is make login page

--- React/recipe-realm-app/src/index.js
+++ React/recipe-realm-app/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LandingPage from './Login/LandingPage'
+import LandingPage from './LandingPage/LandingPage'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/*<App />*/}
       <LandingPage />
   </React.StrictMode>
 );
