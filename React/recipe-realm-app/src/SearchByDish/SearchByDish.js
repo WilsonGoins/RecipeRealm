71eaaa66f43b469dfc512b17b97c965990070f23
WilsonGoins <wilson.goins@ufl.edu>
2024-03-17 00:55:53 -0400

    trying to show options from query in SBD. I cannot get the data and store it in recipeOptions correctly. everything is acting really odd. tomorrow issue

--- React/recipe-realm-app/src/SearchByDish/SearchByDish.js
+++ React/recipe-realm-app/src/SearchByDish/SearchByDish.js
@@ -6,23 +6,24 @@ import QuestionMark_RR from "./QuestionMark_RR.png"
 
 const SearchByDish = () => {
     const [query, setQuery] = useState("")
+    const [recipeOptions, setRecipeOptions] = useState([])      // a list of recipes (each recipe is a dict)
     const helpText = "Enter any common dish, and we will search the internet for recipes that match it. If we don't find anything you like, try finding a recipe online and then use our 'Find With URL' page!"
 
-    const GetRecipeInfo = async (event) => {
+    const GetRecipeOptions = async (event) => {
         event.preventDefault();
         try {
             const endpoint = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${SPapi}`;
             const response = await fetch(endpoint);
 
-            // check if we got a good response
+            // check if we got a bad response
             if (!response.ok) {
                 ShowAlert("Uh-Oh, Something Went Wrong", "");
             } else {   // if we didn't, proceed...
-                const rawData = await response.json();
-                const data = rawData.results || [];
+                const data = await response.json();
                 if (data.length !== 0) {         // if we didn't get an empty result
-                    // TODO: do something with the list of recipes
+                    setRecipeOptions(data);
                 } else {            // if we did, show them a message
+                    setRecipeOptions([])
                     ShowAlert("Sorry No Results For This Query!", "Try something less specific")
                 }
             }
@@ -59,7 +60,7 @@ const SearchByDish = () => {
                            value={query}/>
 
                     <button className="btn btn-outline-success bg-dark" type="button" style={{color: "antiquewhite"}}
-                            onClick={(event) => GetRecipeInfo(event)}>
+                            onClick={(event) => GetRecipeOptions(event)}>
                         Search
                     </button>
                 </form>
@@ -70,6 +71,27 @@ const SearchByDish = () => {
                 <div className="SBD-title-text">
                     Search By Dish
                 </div>
+            </div>
+
+            {/* recipe options */}
+            <div>
+                {/* this is how we have to do conditional statements */}
+                {recipeOptions.length === 0 && (          // if we have options to choose from
+                    <>
+                        <div className="SBD-options-title-container">
+                            <div className="SBD-options-title-text">Options</div>
+                        </div>
+
+                        <div>
+                            {recipeOptions.map((recipe, index) => (
+                                <div key={index} className="SBD-options-items-container">
+                                    <div className="SBD-options-items-text">Title: {recipe.title}</div>
+                                    <div className="SBD-options-items-text">ID: {recipe.id}</div>
+                                </div>
+                            ))}
+                        </div>
+                    </>
+                )}
             </div>
 
             {/* help image */}
