532665316ac7d8634d33025661dfd401eb54f7af
WilsonGoins <wilson.goins@ufl.edu>
2024-04-12 18:29:08 -0400

    made good progress on home page. need to add in actual images to see if I formatted stuff right

--- recipe-realm-app/src/Home/Home.js
+++ recipe-realm-app/src/Home/Home.js
@@ -9,13 +9,35 @@ const Home = () => {
     const navigate = useNavigate();
     const [name, setName] = useState("");
     const [recipes, setRecipes] = useState([]);
+    const nums = [0, 1, 2];
 
 
     const GetUserInfo = () => {         // TODO: retrieve name and recipes from backend
         setName("Wilson");
-        setRecipes([]);                 // if they have no recipes
+
+        var recipesResult = []
+        var resFromBackEnd = [{"title": "Chicken", "url": "???.com", "time": 80, "servings": 44, "steps": ["uhhhh idk", "chicken?"], "ingredients":[{"name": "1 cup of water", "aisle": "water"}, {"name": "2 chickens of chicken", "aisle": "meats"}]}];                 // if they have no recipes
+        for (var i = 0; i < resFromBackEnd.length; i += 3) {
+            var tempRes = [];
+            for (var j = 0; j < 3; j++) {
+                tempRes.push(resFromBackEnd[i + j]);
+            }
+            recipesResult.push(tempRes);
+        }
+
+        console.log(recipesResult);
+        setRecipes(recipesResult);
+        // so recipes is a 2d list. the outer list is groups of 3, the inner lists has individual recipes
     }
 
+    const ReduceRecipeName = (title) => {
+        if (title.length > 25) {
+            return title.substring(0, 25) + '...';
+          } else {
+            return title;
+        }    
+    }
+    
     useEffect(() => {       // this get's called as soon as we open this page
         GetUserInfo();
     }, []);
@@ -24,38 +46,53 @@ const Home = () => {
         <>
             <Template />
 
-            <div className="HM-title-container">
+            <div className="HM-container">
                 <div className="HM-title-text">
-                    Welcome to your RecipeRealm™, {name}
+                    {name}'s RecipeRealm™
                 </div>
+
+                {/* user's recipes */}
+                <div>
+                    {recipes.length !== 0 && (                // if they selected a recipe
+                        <>
+                            <div className="HM-heading-container">
+                                <div className="HM-heading-text">
+                                    Your Recipes:
+                                </div>
+                            </div>
+
+                            <div className="HM-all-recipes-container">
+                                {recipes.map((list) => (                // go through the 1st dimension of the list (groups of 3)
+                                    <div className="HM-row-recipes-container">
+                                        {list.map((recipe) => (         // go through each recipe in group of 3
+                                            recipe && recipe.title && (
+                                                <div className="HM-singular-recipe-container">
+                                                    <div className="HM-recipe-text"> {ReduceRecipeName(recipe.title)} </div>
+                                                    <img className="HM-recipe-img" src={recipe.image} alt="Picture of Dish" />                                                </div>
+                                            )
+                                        ))}
+                                    </div>
+                                ))} 
+                            </div>
+                        </>
+                    )}
+                </div>  
             </div>
 
-            {/* user's recipes */}
-            <div>
-                {recipes.length !== 0 && (                // if they selected a recipe
-                    <>
-                        <div className="HM-heading-container">
-                            <div className="HM-heading-text">
-                                Your Recipes:
-                            </div>
-                        </div>
-                    </>
-                )}
+            {/* this doesn't need to be inside the div */}
+            {recipes.length === 0 && (
+                <>
+                    <div className="HM-sad-chef-container">
+                        <img src={SadChef_RR} alt="Dissapointed Chef" className="HM-sad-chef-img"/>
+                    </div>
 
-                {recipes.length === 0 && (
-                    <>
-                        <div className="HM-sad-chef-container">
-                            <img src={SadChef_RR} alt="Dissapointed Chef" className="HM-sad-chef-img"/>
+                    <div className="HM-sad-text-container">
+                        <div className="HM-sad-text">
+                            You haven't added any recipes yet ☹
                         </div>
-
-                        <div className="HM-sad-text-container">
-                            <div className="HM-sad-text">
-                                You haven't added any recipes yet ☹
-                            </div>
-                        </div>
-                    </>
-                )}
-            </div>  
+                    </div>
+                </>
+            )}
         </>
     )
 }
