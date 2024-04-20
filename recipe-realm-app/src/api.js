fabc2e98135315be473945ffe3cbb413a11995e3
WilsonGoins <wilson.goins@ufl.edu>
2024-04-20 16:49:00 -0400

    changed baseurl in api.js

--- recipe-realm-app/src/api.js
+++ recipe-realm-app/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    // baseURL: 'http://localhost:8000',
+    baseURL: 'https://reciperealm-backend-ff4e853245cd.herokuapp.com/',
 });
 
 export default api;
