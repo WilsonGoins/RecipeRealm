7e9c4738d4bf7fc21f03e923879711a9511ffa98
WilsonGoins <wilson.goins@ufl.edu>
2024-04-20 18:51:09 -0400

    back to square 1, I'm so lost

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
