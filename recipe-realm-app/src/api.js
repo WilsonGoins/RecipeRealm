f78cca7d0a810269f71af820ef7e9ff4ced26c5e
WilsonGoins <wilson.goins@ufl.edu>
2024-04-22 12:35:08 -0400

    updated for deployment

--- recipe-realm-app/src/api.js
+++ recipe-realm-app/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://reciperealm-backend-ff4e853245cd.herokuapp.com/'
 });
 
 export default api;
