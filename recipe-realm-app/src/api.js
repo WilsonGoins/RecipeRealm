3981dc98f0103ee019b53dbacb36040436025e3b
WilsonGoins <wilson.goins@ufl.edu>
2024-04-22 18:01:55 -0400

    changed baseURL to match deployment on render

--- recipe-realm-app/src/api.js
+++ recipe-realm-app/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://reciperealm-backend-ff4e853245cd.herokuapp.com/'
+    // baseURL: 'https://reciperealm-backend-ff4e853245cd.herokuapp.com/'
+    baseURL: 'https://reciperealm-backend-2i9s.onrender.com/'
 });
 
 export default api;
