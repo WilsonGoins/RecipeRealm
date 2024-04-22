fa8f26f0a55e78bd47da1edd4b007ac8124f4b97
WilsonGoins <wilson.goins@ufl.edu>
2024-04-22 18:21:33 -0400

    removed '/' at end of api.js baseURL link

--- recipe-realm-app/src/api.js
+++ recipe-realm-app/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://reciperealm-backend-ff4e853245cd.herokuapp.com/'
+    baseURL: 'https://reciperealm-backend-ff4e853245cd.herokuapp.com'
 });
 
 export default api;
