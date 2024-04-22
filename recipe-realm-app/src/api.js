2ccdc6fe2ad299a4ea5b6ba8950a13e8af12d1d5
WilsonGoins <wilson.goins@ufl.edu>
2024-04-22 18:04:37 -0400

    reverted commit

--- recipe-realm-app/src/api.js
+++ recipe-realm-app/src/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://reciperealm-backend-ff4e853245cd.herokuapp.com/'
-    baseURL: 'https://reciperealm-backend-2i9s.onrender.com/'
+    baseURL: 'https://reciperealm-backend-ff4e853245cd.herokuapp.com/'
 });
 
 export default api;
