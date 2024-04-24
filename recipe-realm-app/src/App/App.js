d2740ec5c88338f3cf86f3d9397b45b45af00533
ethand23 <116743723+ethand23@users.noreply.github.com>
2024-04-24 13:37:36 -0400

    Couple of changes and comments

--- recipe-realm-app/src/App/App.js
+++ recipe-realm-app/src/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
-import LandingPage from '../LandingPage/LandingPage'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'; // Importing necessary components from react-router-dom
+import LandingPage from '../LandingPage/LandingPage' // Importing components for different routes
 import CreateAccount from "../CreateAccount/CreateAccount";
 import Login from '../Login/Login';
 import Home from '../Home/Home';
@@ -13,14 +13,14 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" exact element={<LandingPage />} />
-                <Route path="/createaccount" exact element={<CreateAccount />} />
-                <Route path="/login" exact element={<Login />} />
-                <Route path="/home" exact element={<Home />} />
-                <Route path="/searchbydish" exact element={<SearchByDish />} />
-                <Route path="/findwithurl" exact element={<FindWithURL />} />
-                <Route path="/buildyourown" exact element={<BuildYourOwn />} />
-                <Route path="/shoppinglists" exact element={<ShoppingLists />} />
+                <Route path="/" exact element={<LandingPage />} />  //route for the landing page
+                <Route path="/createaccount" exact element={<CreateAccount />} />   //route for create account
+                <Route path="/login" exact element={<Login />} />   //route for login
+                <Route path="/home" exact element={<Home />} /> //route to home
+                <Route path="/searchbydish" exact element={<SearchByDish />} /> //route for search by dish
+                <Route path="/findwithurl" exact element={<FindWithURL />} />   //route for find with url
+                <Route path="/buildyourown" exact element={<BuildYourOwn />} /> //route for build your own
+                <Route path="/shoppinglists" exact element={<ShoppingLists />} />   //route for shopping lists
             </Routes>
         </BrowserRouter>
     );
